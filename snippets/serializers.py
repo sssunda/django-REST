5b23d128a1b899fd5ec50f33f7a709cc7b6a18c1
sssunda <sundaeun93@gmail.com>
2019-05-31 16:34:13 +0900

    Complete tutorial ch05

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -5,16 +5,19 @@ from django.contrib.auth.models import User
 # Serializer class는 Django의 Form Class와 매우 유사하다.
 class SnippetSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
+        fields = ('url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style')
     
 class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'snippets')
+        fields = ('url', 'id', 'username', 'snippets')
 
 
     
